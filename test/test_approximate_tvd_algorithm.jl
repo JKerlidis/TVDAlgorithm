21771ccda5b10f48c768007a3bf41ce7b29e4545
JKerlidis <jamesk@ozemail.com.au>
2023-03-03 22:10:00 +1100

    regularise test naming

--- test/test_approximate_tvd_algorithm.jl
+++ test/test_approximate_tvd_algorithm.jl
@@ -13,7 +13,7 @@ bin_probs = [Distributions.pdf(Distributions.Binomial(3, 0.5), c) for r ∈ 0:3,
     @test CensoredObservation(3, 1) + CensoredObservation(5, 0) == CensoredObservation(8, 1)
 end
 
-@testset "Test simulate_coupling_probability" begin
+@testset "Test simulate_coupling_probability function" begin
     @test_throws DimensionMismatch simulate_coupling_probability(
         rng, 1, 10, unif_probs, [0.25 for r ∈ 0:4, c ∈ 0:3]
     )
@@ -24,7 +24,7 @@ end
     end
 end
 
-@testset "Test approximate_tvd" begin
+@testset "Test approximate_tvd function" begin
     @test round(approximate_tvd(rng, 10000000, 1, 3, unif_probs, bin_probs), digits=3) ==
           round(approximate_tvd(rng, 10000000, 1, 3, bin_probs, unif_probs), digits=3)
     @test round(approximate_tvd(rng, 10000000, 1, 3, unif_probs, bin_probs), digits=3) == 0.344
