bdaf26b4081fa4d459b052a262583fdfe8f3410d
JKerlidis <jamesk@ozemail.com.au>
2023-03-25 12:13:44 +1100

    Add option to fix s

--- src/processes/psdbp_matching_k_geometric_offspring.jl
+++ src/processes/psdbp_matching_k_geometric_offspring.jl
@@ -1,29 +1,33 @@
 # A population-size-dependent branching process (PSDBP) with negative binomial
 # offspring distribution such that the PSDBP has a carrying capacity of K ∈ ℕ₁,
 # and such that the mean and variance of the one-step distributions match that
-# of the CBPKGeometricOffspring model
+# of the CBPKGeometricOffspring model. If `fix_s` is set to true, only the mean
+# will match, with the `s` parameter of the NB distribution kept constant
 struct PSDBPMatchingKGeometricOffspring <: BranchingProcess
     K::Integer
     M::Integer
     max_z::Integer
+    fix_s::Bool
 
     function PSDBPMatchingKGeometricOffspring(
         K::Integer,
         M::Integer,
-        max_z::Integer
+        max_z::Integer,
+        fix_s::Bool=false
     )
         K ≥ 0 || throw(DomainError(K, "argument must be non-negative"))
         M ≥ 0 || throw(DomainError(M, "argument must be non-negative"))
         max_z ≥ 0 || throw(DomainError(max_z, "argument must be non-negative"))
 
-        new(K, M, max_z)
+        new(K, M, max_z, fix_s)
     end
 end
 
 PSDBPMatchingKGeometricOffspring(
     K::Integer,
-    M::Integer
-) = PSDBPMatchingKGeometricOffspring(K, M, max(3 * K, 30))
+    M::Integer,
+    fix_s::Bool=false
+) = PSDBPMatchingKGeometricOffspring(K, M, max(3 * K, 30), fix_s)
 
 PSDBPMatchingKGeometricOffspring(
     K::Integer
@@ -37,7 +41,8 @@ function transition_probabilities(
     # Define the specific negative binomial distribution generating
     # Zₙ = ∑_{i=1}^z ξ(z)
     r(z, K, M) = ((z + M) * K^2) / (2(K + M) * z + 2K * M + K^2)
-    s(z, K, M) = ((z + K) * (K + M)) / (5(K + M) * z + 5K * M + 3K^2)
+    s(z, K, M) = d.fix_s ? (K * (K + M)) / (8K + 10M) :
+                 ((z + K) * (K + M)) / (5(K + M) * z + 5K * M + 3K^2)
     Zₙ(z) = Distributions.NegativeBinomial(r(z, d.K, d.M), s(z, d.K, d.M))
 
     P = Array{Float64}(undef, d.max_z + 1, d.max_z + 1)
@@ -63,7 +68,8 @@ function log_likelihood(
     # Define the specific negative binomial distribution generating
     # (Zₙ|Zₙ₋₁=z) = ∑_{i=1}^z ξ(z)
     r(z, K, M) = ((z + M) * K^2) / (2(K + M) * z + 2K * M + K^2)
-    s(z, K, M) = ((z + K) * (K + M)) / (5(K + M) * z + 5K * M + 3K^2)
+    s(z, K, M) = d.fix_s ? (K * (K + M)) / (8K + 10M) :
+                 ((z + K) * (K + M)) / (5(K + M) * z + 5K * M + 3K^2)
     Zₙ(z) = Distributions.NegativeBinomial(r(z, d.K, d.M), s(z, d.K, d.M))
 
     # Return the sum of negative binomial log-likelihoods, one for each
