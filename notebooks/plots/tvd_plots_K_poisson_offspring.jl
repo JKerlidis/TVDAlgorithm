ee1fcd4eaca0ed1b16ecee41b3cc86fd30aec9f7
JKerlidis <jamesk@ozemail.com.au>
2023-07-03 22:30:22 +1000

    Stacked K & path length plot

--- notebooks/plots/tvd_plots_K_poisson_offspring.jl
+++ notebooks/plots/tvd_plots_K_poisson_offspring.jl
@@ -62,28 +62,6 @@ tvd_results_mean_only_z0_is_1 = Unmarshal.unmarshal(
     JSON.parse(data)
 )
 
-
-zero_inflation_plot = plot(
-    1:100,
-    i -> zero_inflation_tvd_results.results[11, i],
-    title="Estimated one-step TVD over z_0, for K=100, M=2, and λ=3",
-    xlabel="Initial population size",
-    ylabel="Estimated TVD", linecolor=:purple,
-    linealpha=0.9,
-    linewidth=3,
-    legend=false,
-    titlefontsize=48,
-    tickfontsize=32,
-    guidefontsize=42,
-    size=(2800, 2000),
-    ylims=(0, 0.08),
-    bottom_margin=28mm,
-    left_margin=28mm,
-    top_margin=6mm
-)
-
-savefig(zero_inflation_plot, "out/plots/poisson_offspring/zero_inflation.png")
-
 zero_inflation_comparative_plot = plot(
     1:100,
     [
@@ -122,28 +100,6 @@ zero_inflation_comparative_plot = plot(
 )
 
 savefig(zero_inflation_comparative_plot, "out/plots/poisson_offspring/zero_inflation_comparative.png")
-
-path_length_plot = plot(
-    1:50,
-    i -> tvd_results_z0_is_1.results[i, 10].mean,
-    title="Estimated TVD over different path lengths, for K=100 and z₀=1",
-    xlabel="Path length",
-    ylabel="Estimated TVD",
-    linecolor=:purple,
-    linealpha=0.9,
-    linewidth=3,
-    legend=false,
-    titlefontsize=48,
-    tickfontsize=32,
-    guidefontsize=42,
-    size=(2800, 2000),
-    ylims=(0, 0.18),
-    bottom_margin=28mm,
-    left_margin=28mm,
-    top_margin=6mm
-)
-
-savefig(path_length_plot, "out/plots/poisson_offspring/tvd_path_length.png")
 
 comparative_path_length_plot = plot(
     1:50,
@@ -198,32 +154,6 @@ mean_only_path_length_plot = plot(
 )
 
 savefig(mean_only_path_length_plot, "out/plots/poisson_offspring/tvd_path_length_mean_only.png")
-
-K_plot = plot(
-    i -> tvd_results_z0_is_1.K_vals[i],
-    i -> tvd_results_z0_is_1.results[30, i].mean,
-    1:20,
-    title="Estimated TVD over different K, for path length 30 and z₀=1",
-    xlabel="Carrying capacity",
-    ylabel="Estimated TVD",
-    linecolor=:purple,
-    linealpha=0.9,
-    linewidth=3,
-    legend=false,
-    titlefontsize=48,
-    tickfontsize=32,
-    guidefontsize=42,
-    size=(2800, 2000),
-    xticks=0:20:200,
-    ylims=(0, 0.45),
-    fillcolour=:thistle,
-    fillalpha=0.4,
-    bottom_margin=28mm,
-    left_margin=28mm,
-    top_margin=6mm
-)
-
-savefig(K_plot, "out/plots/poisson_offspring/tvd_K.png")
 
 comparative_K_plot = plot(
     i -> tvd_results_z0_is_1.K_vals[i],
@@ -281,7 +211,7 @@ mean_only_K_plot = plot(
 
 savefig(mean_only_K_plot, "out/plots/poisson_offspring/tvd_K_mean_only.png")
 
-K_plot_multiple_path_lengths = plot(
+K_plot_multiple_path_lengths_z0_is_1 = plot(
     i -> tvd_results_z0_is_1.K_vals[i],
     [
         i -> tvd_results_z0_is_1.results[5, i].mean,
@@ -294,28 +224,62 @@ K_plot_multiple_path_lengths = plot(
         i -> tvd_results_z0_is_1.results[40, i].mean,
     ],
     1:20,
-    title="Estimated TVD over different K, for z₀=1 and different path lengths",
+    title="Initial Population Size 1",
+    titlefont=font(32, "Computer Modern"),
+    fontfamily="Computer Modern",
     xlabel="Carrying capacity",
     ylabel="Estimated TVD",
-    linealpha=0.9,
-    linewidth=3,
+    legend=false,
+    xticks=0:50:200,
+    ylims=(0, 0.45),
+    palette=palette([:purple, :thistle, :skyblue, :olivedrab], 8),
+)
+
+K_plot_multiple_path_lengths_z0_is_K = plot(
+    i -> tvd_results_z0_is_K.K_vals[i],
+    [
+        i -> tvd_results_z0_is_K.results[5, i].mean,
+        i -> tvd_results_z0_is_K.results[10, i].mean,
+        i -> tvd_results_z0_is_K.results[15, i].mean,
+        i -> tvd_results_z0_is_K.results[20, i].mean,
+        i -> tvd_results_z0_is_K.results[25, i].mean,
+        i -> tvd_results_z0_is_K.results[30, i].mean,
+        i -> tvd_results_z0_is_K.results[35, i].mean,
+        i -> tvd_results_z0_is_K.results[40, i].mean,
+    ],
+    1:20,
+    title="Initial Population Size K",
+    titlefont=font(32, "Computer Modern"),
+    fontfamily="Computer Modern",
+    xlabel="Carrying capacity",
+    ylabel="Estimated TVD",
     legend=:topright,
     labels=hcat("path length 5", "path length 10", "path length 15", "path length 20",
         "path length 25", "path length 30", "path length 35", "path length 40"),
-    legendfontsize=32,
-    titlefontsize=48,
-    tickfontsize=32,
-    guidefontsize=42,
-    size=(2800, 2000),
-    xticks=0:20:200,
+    legendfontsize=28,
+    xticks=0:50:200,
     ylims=(0, 0.45),
-    palette=palette([:purple, :lavender, :skyblue, :olivedrab], 8),
-    fillalpha=0.4,
-    bottom_margin=28mm,
-    left_margin=28mm,
-    top_margin=6mm
+    palette=palette([:purple, :thistle, :skyblue, :olivedrab], 8),
 )
 
-savefig(K_plot_multiple_path_lengths, "out/plots/poisson_offspring/tvd_K_and_path_length.png")
+stacked_K_and_path_length_plot = plot(
+    K_plot_multiple_path_lengths_z0_is_1,
+    K_plot_multiple_path_lengths_z0_is_K,
+    layout=(1, 2),
+    linealpha=0.9,
+    linewidth=3,
+    plot_title="Estimated TVD over various K and various path lengths",
+    fontfamily="Computer Modern",
+    plot_titlefontsize=48,
+    tickfontsize=32,
+    guidefontsize=32,
+    size=(3200, 1600),
+    bottom_margin=28mm,
+    left_margin=28mm,
+    top_margin=0mm
+)
+
+
+savefig(stacked_K_and_path_length_plot, "out/plots/poisson_offspring/tvd_stacked_K_and_path_length_plot.png")
 
 end
